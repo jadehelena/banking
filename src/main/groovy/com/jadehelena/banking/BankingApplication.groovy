80a8e16feebd0ac5a85cb5d2fd56088f8a461f90
jadehelena <jademoraes@gmail.com>
2021-04-22 18:31:53 -0300

    🎉 Starting project

---
+++ src/main/groovy/com/jadehelena/banking/BankingApplication.groovy
@@ -0,0 +1,13 @@
+package com.jadehelena.banking
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BankingApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(BankingApplication, args)
+	}
+
+}
