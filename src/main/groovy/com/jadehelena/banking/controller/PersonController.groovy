c18efe5ed4034b307a63fc2beadd3b95da8e0d55
jadehelena <jademoraes@gmail.com>
2021-04-22 22:17:33 -0300

    ✨ Adds PUT, GET and DELETE people

--- src/main/groovy/com/jadehelena/banking/controller/PersonController.groovy
+++ src/main/groovy/com/jadehelena/banking/controller/PersonController.groovy
@@ -8,20 +8,45 @@ import javax.transaction.Transactional
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.PathVariable
 
 @RestController
-@RequestMapping('persons')
+@RequestMapping('people')
 class PersonController {
 
     @Autowired
     PersonService personService
 
+    @GetMapping
+    List findAll() {
+        personService.findAll()
+    }
+
+    @GetMapping('{id}')
+    Person findOne(@PathVariable long id) {
+        personService.findById(id)
+    }
+
     @PostMapping
     @Transactional
     Person save(@RequestBody Person person) {
         personService.save(person)
+    }
+
+    @PutMapping('{id}')
+    @Transactional
+    Person update(@RequestBody Person person, @PathVariable long id) {
+        personService.update(person, id)
+    }
+
+    @DeleteMapping('{id}')
+    Person deleteById(@PathVariable long id) {
+        personService.deleteById(id)
     }
 
 }
--- src/main/groovy/com/jadehelena/banking/service/PersonService.groovy
+++ src/main/groovy/com/jadehelena/banking/service/PersonService.groovy
@@ -4,14 +4,48 @@ import com.jadehelena.banking.model.Person
 import com.jadehelena.banking.repository.PersonRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.data.domain.Sort
+import javax.persistence.EntityNotFoundException
 
 @Service
 class PersonService {
     @Autowired
-    PersonRepository PersonRepository
+    PersonRepository personRepository
+
+    List findAll() {
+        personRepository.findAll(Sort.by('name')).asList()
+    }
+
+    Person findById(long id) {
+        personRepository.findById(id).orElse(null)
+    }
+
+    Person findByIdOrError(long id) {
+        personRepository.findById(id).orElseThrow({
+            new EntityNotFoundException()
+        })
+    }
 
     def save(Person person){
-        PersonRepository.save(person)
+        personRepository.save(person)
+    }
+
+    def update(Person person, long id) {
+        Person persistedPerson = findByIdOrError(id)
+
+        persistedPerson.with {
+            name = person.name
+            lastname = person.lastname
+            document = person.document
+        }
+
+        personRepository.save(persistedPerson)
+    }
+
+    def deleteById(long id) {
+        Person person = findByIdOrError(id)
+        personRepository.delete(person)
+        person
     }
 
 }
