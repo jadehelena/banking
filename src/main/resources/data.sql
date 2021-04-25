3a6cb750be1f9851d3d918f8874d00f8f9e16791
jadehelena <jademoraes@gmail.com>
2021-04-25 16:49:34 -0300

    :seedling: Set default role and user on data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,3 @@
-INSERT INTO USER(name, password) VALUES ('admin', '$2a$10$sFKmbxbG4ryhwPNx/l3pgOJSt.fW1z6YcUnuE2X8APA/Z3NI/oSpq');
+INSERT INTO ROLE(id, name) VALUES (1, 'admin');
+INSERT INTO USER(id, name, password) VALUES (1, 'admin', '$2a$10$sFKmbxbG4ryhwPNx/l3pgOJSt.fW1z6YcUnuE2X8APA/Z3NI/oSpq');
+INSERT INTO USER_ROLES(user_id, roles_id) VALUES (1, 1);
