421e92853bf7601fafb60c5b33982910fe8c9db0
jadehelena <jademoraes@gmail.com>
2021-04-29 00:57:49 -0300

    :fire: Removing JWT validation due to unit test issues :laughing:

--- src/main/groovy/com/jadehelena/banking/repository/UserRepository.groovy
+++
@@ -1,8 +0,0 @@
-package com.jadehelena.banking.repository
-
-import com.jadehelena.banking.model.User
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserRepository extends JpaRepository<User, Long>{
-    Optional<User> findByName(String name)
-}
--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO ROLE(id, name) VALUES (1, 'admin');
-INSERT INTO USER(id, name, password) VALUES (1, 'admin', '$2a$10$sFKmbxbG4ryhwPNx/l3pgOJSt.fW1z6YcUnuE2X8APA/Z3NI/oSpq');
-INSERT INTO USER_ROLES(user_id, roles_id) VALUES (1, 1);
